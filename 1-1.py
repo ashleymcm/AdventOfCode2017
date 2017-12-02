47d440d639ec0d5787bda21baf0c4715a0eda129
Ashley Terwilliger <ashley.terwil@gmail.com>
2017-12-01 23:11:58 -0500

    Day one, wooo!
    PS - Still unconvinced Python is not garbage but this was really easy to
    do.

---
+++ 1-1.py
@@ -0,0 +1,17 @@
+def captcha_sum(captcha):
+    # define sum and set it to nada
+    sum = 0
+
+    # take first character and add to end since it "wraps"
+    captcha += captcha[0]
+
+    # cycle through string checking for dupes
+    index = 0
+    while index < len(captcha) - 1:
+        if captcha[index] == captcha[index + 1]:
+            # add to sum if it's a dupe
+            sum += int(captcha[index])
+        index = index + 1
+
+    # return sum
+    return sum
---
+++ 1-2.py
@@ -0,0 +1,19 @@
+def captcha_sum(captcha):
+    # define sum and set it to nada
+    sum = 0
+    length = len(captcha)
+    half_length = len(captcha)//2
+
+    # add half the string again to the end since it "wraps"
+    captcha += captcha[0:half_length]
+
+    # cycle through string checking for dupes
+    index = 0
+    while index < length:
+        if captcha[index] == captcha[index + half_length]:
+            # add to sum if it's a dupe
+            sum += int(captcha[index])
+        index = index + 1
+
+    # return sum
+    return sum
