172b0693b164ab386cf9967351745a1ae4068e62
Ashley Terwilliger <ashley.terwil@gmail.com>
2017-12-04 20:36:38 -0500

    "collections" and "itertools" made this really easy, most of my code is comments. Whoups.

---
+++ 4-1.py
@@ -0,0 +1,18 @@
+def check_valid_passwords():
+    count = 0
+
+    # take the spreadsheet from the file and split it in an array of "passwords"
+    with open("input.txt") as password_list:
+        passwords = [passwords.split() for passwords in password_list]
+
+    # loop through all the passwords
+    for password in passwords:
+        # sets are collections of unique elements so if we turn the array
+        # into a set and the length is different than the original we know
+        # that there was a duplicate in there
+        if len(password) == len(set(password)):
+            count = count + 1
+
+    return count
+
+print(check_valid_passwords())
---
+++ 4-2.py
@@ -0,0 +1,30 @@
+from collections import Counter
+import itertools
+
+
+def check_valid_passwords():
+    count = 0
+
+    # take the spreadsheet from the file and split it in an array of "passwords"
+    with open("input.txt") as password_list:
+        passwords = [passwords.split() for passwords in password_list]
+
+    # loop through all the passwords
+    for password in passwords:
+        valid = True
+        # use itertools to compare each array element to every other array element
+        for a, b in itertools.combinations(password, 2):
+            # Counter will count the instances of each char in the strings
+            if Counter(a) == Counter(b):
+                # if they are the same then it's an anagram and we set valid to False
+                valid = False
+                break # we also break out of the loop to save time
+
+        if valid:
+            # add to the count if it's valid
+            count = count + 1
+
+
+    return count
+
+print(check_valid_passwords())
